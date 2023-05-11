b09d974a4bcd2f078d197b88079c0e0a92581152
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-05-11 19:30:02 +0530

    commit
    commit

---
+++ app/Http/Controllers/profile/AvatarController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller
+{
+
+    public function update(UpdateAvatarRequest $request)
+    {
+
+
+
+        $path = $request->file('avatar')->store('avatars', 'public');
+
+        auth()->user()->update(['avatar' =>  $path]);
+        return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\profile\AvatarController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/about', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/', 'home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
